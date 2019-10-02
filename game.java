1ad3aedafb0dd95da9e106f26c741a1a78141dae
TimDeanMoser <tim.moser@uzh.ch>
2019-10-02 16:56:58 +0200

    Update game.java

---
+++ game.java
@@ -0,0 +1,16 @@
+public class game
+{
+	int number_of_squares;
+	int number_of_players;
+	boolean win;
+	gameboard my_gameboard;
+
+	public game(int number_of_squares, int number_of_players) 
+	{ 
+		this.number_of_squares = number_of_squares;
+		this.number_of_players = number_of_players;
+		this.win = false;
+		this.my_gameboard=new gameboard();
+		my_gameboard.initialize(number_of_squares);
+	}
+}
