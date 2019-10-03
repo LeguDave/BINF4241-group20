9fbccf6bcdb2ab5b0d7c1a7cb12753cff8623d8d
TimDeanMoser <tim.moser@uzh.ch>
2019-10-03 10:19:54 +0200

    Update normal_square.java

--- normal_square.java
+++ normal_square.java
@@ -1,11 +1,10 @@
-public class normal_square
+public class normal_square extends square
 {
-	int position;
-	boolean occupied;
 
-	public snake_square(int position) 
+	public normal_square(int position) 
 	{ 
 		this.position = position;
 		this.occupied = false;
+		this.type="normal_square";
 	}
 }
