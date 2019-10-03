9adcdaca4433823a8c6e4fee0bffa4bd5ecb88ec
TimDeanMoser <tim.moser@uzh.ch>
2019-10-03 10:21:01 +0200

    Update snake_square.java

--- snake_square.java
+++ snake_square.java
@@ -1,13 +1,12 @@
-public class snake_square
+public class snake_square extends square
 {
-	int position;
 	int steps_back;
-	boolean occupied;
 
 	public snake_square(int position, int steps_back) 
 	{ 
 		this.position = position;
 		this.steps_back = steps_back;
 		this.occupied = false;
+		this.type="snake_square";
 	}
 }
