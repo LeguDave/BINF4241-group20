3cde143d719274ba4145ce1fc737be99d05961d3
TimDeanMoser <tim.moser@uzh.ch>
2019-10-03 15:10:42 +0200

    Update game.java
    implemented game functionalities and overview

--- game.java
+++ game.java
@@ -1,15 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class game
 {
-	int number_of_squares;
-	int number_of_players;
-	boolean win;
-	gameboard my_gameboard;
+	int squares;
+	gameboard gameboard;
+   ArrayList<player> players = new ArrayList<player>();
 
-	public game(int number_of_squares, int number_of_players) 
+
+	public game(int squares) 
 	{ 
-		this.number_of_squares = number_of_squares;
-		this.number_of_players = number_of_players;
-		this.win = false;
-		this.my_gameboard=new gameboard(20);
+		this.squares = squares;
+		this.gameboard=new gameboard(squares);
 	}
+   
+   public void add_player(String name)
+   {
+     this.players.add(new player(name));
+   }
+   
+   public void take_turn(player player)
+   {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println(player.name + " it's your turn! Press enter to move.");
+      String line = scanner.nextLine();
+      System.out.println(player.move(die.roll(), this.gameboard));    
+   }
+   
+   public void run_game()
+   {
+      if(this.players.size()==0)
+      {
+         System.out.println("You haven't added any players yet! \n");
+         return;
+      }
+      
+      int i=0;
+      while(i<200)
+      {
+         take_turn(this.players.get(i % this.players.size()));
+         if(this.gameboard.field[this.squares-1].occupied==true)
+         {
+            System.out.println("GAME OVER. "+(this.players.get(i)).name+" won");
+            return;
+         }
+         i+=1;       
+      }
+      
+   }
+   
 }
