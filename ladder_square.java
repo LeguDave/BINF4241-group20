3ac6eb8be869b4c9d46cbc1bb6cc0f0eec866f95
TimDeanMoser <tim.moser@uzh.ch>
2019-10-03 10:19:42 +0200

    Update ladder_square.java

--- ladder_square.java
+++ ladder_square.java
@@ -1,13 +1,13 @@
-public class ladder_square
+public class ladder_square extends square
 {
-	int position;
 	int steps_forward;
-	boolean occupied;
 
 	public ladder_square(int position, int steps_forward) 
 	{ 
 		this.position = position;
 		this.steps_forward = steps_forward;
 		this.occupied = false;
+		this.type="ladder_square";
 	}
+	
 }
