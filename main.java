8364fdf28aa02e1025aafdf420f7736cbdb7dc0c
TimDeanMoser <tim.moser@uzh.ch>
2019-10-03 15:11:10 +0200

    Update main.java
    added first test skelleton

--- main.java
+++ main.java
@@ -2,11 +2,23 @@ public class main
 {
    public static void main(String[] args)
 	{
+      /*
       //square array test
 		ladder_square test=new ladder_square(2,3);
 		ladder_square test2=new ladder_square(5,10);
       square[] myboard = {test,test2};
 		System.out.println(((ladder_square)myboard[0]).steps_forward);
+      
+      //player + turn test
+      player testplayer= new player("David");
+      game testgame= new game(20);
+      testgame.turn(testplayer);
+      */
+      game testgame = new game(12);
+      testgame.add_player("Tim");
+      testgame.add_player("David");
+      testgame.add_player("Ashly");
+      testgame.run_game();
 	}
 
 }
