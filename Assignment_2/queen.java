b4cfc499052ca5ab6824125b7b5858236e9a09e6
Phi Kyuubi <david.steiger@uzh.ch>
2019-10-15 16:07:16 +0200

    da queen

--- Assignment_2/queen.java
+++ Assignment_2/queen.java
@@ -8,10 +8,55 @@ public class queen extends piece{
 		this.rank="Q";
 	}
    
-   public boolean is_legal (String String aim){
-      boolean is_valid=False;
-      //loop through all directions until aim is found
-      //if aim is not found return false, if found return true
+   public boolean is_legal (gameboard gameboard, String aim){
+      int direction=8;
+      boolean is_valid;
+      String next_position;
+      
+      while(direction>0){
+         if(direction==8){
+            next_position=gameboard.up(temporary_position);
+            //iterate in a direction until piece, a wall or aim field is found
+            while(gameboard.is_occupied(next_position)==False & next_position!="" & next_position!=aim){
+               next_position=gameboard.up(next_position);
+            }
+            //check if wall is found
+            if(next_position==""){
+               continue;
+            }
+            //check if aim field is found
+            
+                //check if aim field is occipied
+            
+                   //if occupied check if friendly piece --> continue
+                   //else DESTROY THE CHILD and return true
+                   
+                //return true
+                
+            //continue because a found enemy cannot be killed if the aim is not the enemies field
+         else if(direction==7){
+         //lööp
+         }
+         else if(direction==6){
+         //lööp
+         }
+         else if(direction==5){
+         //lööp
+         }
+         else if(direction==4){
+         //lööp
+         }
+         else if(direction==3){
+         //lööp
+         }
+         else if(direction==2){
+         //lööp
+         }
+         else if(direction==1){
+         //lööp
+         }
+         direction-=1;
+      }
       return is_valid;
    }
 }
