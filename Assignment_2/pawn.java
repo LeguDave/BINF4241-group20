e67e95f175c4a7689aba75ac29e3f7126bd458fd
Phi Kyuubi <david.steiger@uzh.ch>
2019-10-16 17:04:58 +0200

    horse might be finished

--- Assignment_2/pawn.java
+++ Assignment_2/pawn.java
@@ -9,9 +9,6 @@ public class pawn extends piece{
 	}
    
    public boolean is_legal (String aim){
-      boolean is_valid=False;
-      //loop through all directions until aim is found
-      //if aim is not found return false, if found return true
-      return is_valid;
+      
    }
 }
