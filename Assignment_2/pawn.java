8ff01b4975db1fecc7dff40637350b2da931e49f
Phi Kyuubi <david.steiger@uzh.ch>
2019-10-17 15:00:25 +0200

    Update pawn.java

--- Assignment_2/pawn.java
+++ Assignment_2/pawn.java
@@ -12,24 +12,28 @@ public class pawn extends piece{
       if(this.white){
          next_position=gameboard.up(this.position);
          if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
-         else{return true;}
+         if(next_position==aim){return true;}
          next_position=gameboard.up(next_position);
-         if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
-         else{return true;}
+         if(is_row(this.position)){
+            if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
+            if(next_position==aim){return true;}
+         }
          next_position=gameboard.up_right(this.position);
          if(next_position==""){return false;}
-         else if(gameboard.is_occupied(next_position} && gameboard.get_piece(next_position).white==False){return true;}
+         else if(gameboard.is_occupied(next_position} && gameboard.get_piece(next_position).white==false){return true;}
          next_position=gameboard.up_left(this.position);
          if(next_position==""){return false;}
-         else if(gameboard.is_occupied(next_position} && gameboard.get_piece(next_position).white==False){return true;}
+         else if(gameboard.is_occupied(next_position} && gameboard.get_piece(next_position).white==false){return true;}
       }
       else{
          next_position=gameboard.down(this.position);
          if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
-         else{return true;}
+         if(next_position==aim){return true;}
          next_position=gameboard.down(next_position);
-         if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
-         else{return true;}
+         if(is_row(this.position)){
+            if(next_position=="" || gameboard.is_occupied(next_position)){return false;}
+            if(next_position==aim){return true;}
+         }
          next_position=gameboard.down_right(this.position);
          if(next_position==""){return false;}
          else if(gameboard.is_occupied(next_position} && gameboard.get_piece(next_position).white){return true;}
