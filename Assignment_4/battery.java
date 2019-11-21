34836d3fa56ad2a30f5d6c80c4cc8b812956d0da
TimDeanMoser <tim.moser@uzh.ch>
2019-11-21 14:37:45 +0100

    battery can charge and empty now

--- Assignment_4/battery.java
+++ Assignment_4/battery.java
@@ -3,17 +3,31 @@ import java.util.*;
 public class battery extends Thread{
 
    device device=null;
+   boolean charging=false;
    
-   public battery(device device){
+   public battery(device device, boolean charging){
       this.device=device;
+      this.charging=charging;
    }
    public void run(){
       try{
-         while(this.device.battery>0){
+      
+         if(this.charging==false){
+            while(this.device.battery>0){
             Thread.sleep(5000);
             this.device.timer=this.device.battery-1;
             System.out.println(this.device.battery);
+            }
          }
+         else{
+            while(this.device.battery<100){
+            Thread.sleep(1000);
+            this.device.timer=this.device.battery+1;
+            System.out.println(this.device.battery);
+            }
+         
+         }
+         
       }
       catch (InterruptedException e){
          //System.out.println("Timer Interrupted");
